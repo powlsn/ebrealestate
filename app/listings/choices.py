cf92f76631c6b91c1e3a64b65b1e1ff33d10fae2
Elmar Bruckmann <e.bruckmann@henova.de>
2025-03-28 22:27:13 +0100

    update index page

---
+++ app/listings/choices.py
@@ -0,0 +1,43 @@
+state_choices = {
+    'BW': 'Baden-Württemberg',
+    'BY': 'Bayern',
+    'BE': 'Berlin',
+    'BB': 'Brandenburg',
+    'HB': 'Bremen',
+    'HH': 'Hamburg',
+    'HE': 'Hessen',
+    'MV': 'Mecklenburg-Vorpommern',
+    'NI': 'Niedersachsen',
+    'NW': 'Nordrhein-Westfalen',
+    'RP': 'Rheinland-Pfalz',
+    'SL': 'Saarland',
+    'SN': 'Sachsen',
+    'ST': 'Sachsen-Anhalt',
+    'SH': 'Schleswig-Holstein',
+    'TH': 'Thüringen',
+}
+
+price_choices = {
+    '100000': '100000 €',
+    '200000': '200000 €',
+    '300000': '300000 €',
+    '400000': '400000 €',
+    '500000': '500000 €',
+    '600000': '600000 €',
+    '700000': '700000 €',
+    '800000': '800000 €',
+    '900000': '900000 €',
+    '1000000': '1000000 €',
+}
+
+bedroom_choices = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+}
--- app/pages/views.py
+++ app/pages/views.py
@@ -3,11 +3,16 @@ from django.shortcuts import render
 from listings.models import Listing
 from realtors.models import Realtor
 
+from listings.choices import price_choices, state_choices, bedroom_choices
+
 def index(request):
     latest_listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
     context = {
-        'latest_listings': latest_listings
+        'latest_listings': latest_listings,
+        'price_choices': price_choices,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
     }
 
     return render(request, 'pages/index.html', context)
