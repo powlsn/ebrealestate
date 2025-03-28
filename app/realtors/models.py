cf0929bb1b92d3fb37b22860b266dd6d74056bb4
Elmar Bruckmann <e.bruckmann@henova.de>
2025-03-28 14:02:33 +0100

    add listing and realtor model

--- app/realtors/models.py
+++ app/realtors/models.py
@@ -1,3 +1,15 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+
+class Realtor(models.Model):
+    name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
+    description = models.TextField(blank=True)
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=60)
+    is_mvp = models.BooleanField(default=False)
+    hire_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
