5df10fcced3467a912152455db7ce43a98cec37d
Elmar Bruckmann <e.bruckmann@henova.de>
2025-03-28 12:47:23 +0100

    add listings and realtors app setup

--- app/app/urls.py
+++ app/app/urls.py
@@ -3,5 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('pages.urls')),
+    path('listings/', include('listings.urls')),
+    path('realtors/', include('realtors.urls')),
     path('admin/', admin.site.urls),
 ]
