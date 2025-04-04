676d08e526b8a501780f5796fa04c56b92bcf6c7
Elmar Bruckmann <e.bruckmann@henova.de>
2025-04-04 20:51:50 +0200

    implement messages for alerts

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import redirect, render
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        print('SUBMITTED register')
+        messages.error(request, 'TEST alert')
         return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
 def login(request):
     if request.method == 'POST':
-        print('SUBMITTED login')
+        messages.error(request, 'TEST alert')
         return redirect('login')
     else:
         return render(request, 'accounts/login.html')
