b00d51a3dfa7e50819700b849a56c94b704c5735
Elmar Bruckmann <e.bruckmann@henova.de>
2025-05-09 17:01:57 +0200

    backend contact

--- app/contacts/views.py
+++ app/contacts/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect
+
+from .models import Contact
+
 
 def contact(request):
-    return render('<h1>Hello</h1>')
+    if request.method == 'POST':
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        realtor_email = request.POST['realtor_email']
+
+        contact = Contact(listing=listing, listing_id=listing_id, name=name, email=realtor_email, phone=phone, message=message, user_id=user_id)
+        contact.save()
+
+        messages.success(request, 'inquery is send')
+        return redirect('contact')
+
+    return
